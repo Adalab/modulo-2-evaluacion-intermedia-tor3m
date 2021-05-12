8c23ba3ff4bbb8733e1499b194bb4914db71b196
tor3m <torres.m3lly@gmail.com>
2021-05-12 17:46:22 +0200

    add counter for main.js

--- js/main.js
+++ js/main.js
@@ -3,27 +3,38 @@
 const button = document.querySelector(".js-button");
 const inputValue = document.querySelector(".js-writeNumber");
 const resultElement = document.querySelector(".js-result");
-const randonNumber = getRandomNumber(100);
+const tryElement = document.querySelector(".js-try");
 
-console.log(randonNumber);
-
+//randon number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+const randonNumber = getRandomNumber(100);
+console.log(randonNumber);
 
-function handlerclickbutton(ev) {
-  ev.preventDefault();
+let counter = 0;
+function counterNumber() {
+  tryElement.innerHTML = `Número de intentos:` + (++counter);
+}
+
+function handlerconditionalsbutton() {
   const putNumber = parseInt(inputValue.value);
   if (putNumber <= 0 || putNumber >= 100) {
     resultElement.innerHTML = `El número debe estar entre 1 y 100`;
-  }else if (putNumber > randonNumber) {
-    resultElement.innerHTML = 'Demasiado alto';
+  } else if (putNumber > randonNumber) {
+    resultElement.innerHTML = "Demasiado alto";
   } else if (putNumber < randonNumber) {
-    resultElement.innerHTML = 'Demasiado bajo';
+    resultElement.innerHTML = "Demasiado bajo";
   } else if (putNumber === randonNumber) {
     resultElement.innerHTML = `Has ganado campeona!!!`;
-  } 
+  }
+}
+
+function handlerclickbutton(ev) {
+  ev.preventDefault();
+  handlerconditionalsbutton();
+  counterNumber();
 }
 
 button.addEventListener("click", handlerclickbutton);
